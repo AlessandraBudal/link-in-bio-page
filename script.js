7513d7e980ce92938f036906f9024d4fa557d68f
AlessandraBudal <budalalessandra1@gmail.com>
2026-09-19 13:14:37 -0300

    .

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function toggleMode() {
   if(html.classList.contains('light')) {
     html.classList.remove('light')
 
-    img.setAttribute("src", "./assets/image.png")
+    img.setAttribute("src", "./assets/WhatsApp Image 2026-09-19 at 12.54.55.jpeg")
    
 
   } else {
     html.classList.add('light')
-    img.setAttribute("src", "./assets/image.jpeg")
+    img.setAttribute("src", "./assets/WhatsApp Image 2026-09-19 at 12.54.55.jpeg")
    
     
   }
